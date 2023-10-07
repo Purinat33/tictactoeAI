963a906f7f970d11cc9658d3950b722c454e884f
Purinat33 <Purinat33@gmail.com>
2023-10-07 23:56:20 +0700

    Board looks better, todo game logic soon

--- board.py
+++ board.py
@@ -6,20 +6,33 @@ def drawBoard(gameState: list):
     #     ['', 'O', ''],
     #     ['', 'X', '']
     # ]
+    top_bottom = '____' * 3
+    print(top_bottom)
     for rows in range(3):
         
         # Loop each cols in rows I
         if rows != 0:
             print()
+            print(top_bottom, end='')
+            print()
         
         for cols in range(3):
+            # 1 cell
+            
+            print('| ', end='')
             if gameState[rows][cols] == '':
-                print(' ', end='\t')
+                print(' ', end=' ')
             elif gameState[rows][cols] == 'X':
-                print('X', end='\t')
+                print('X', end=' ')
             elif gameState[rows][cols] == 'O':
-                print('O', end='\t')
+                print('O', end=' ')
             else:
                 print()
+            
+            if cols == 2:
+                print('| ', end='')     
+            # / 1 cell
+    print('\n' + top_bottom)
+    
             
             
